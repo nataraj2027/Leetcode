be5484c8cc2424de0886f72e6f10ddb48929b39b
Nataraj M <natarajm9876@gmail.com>
2026-08-26 14:37:10 +0530

    Time: 5 ms (62.31%), Space: 44.6 MB (60.11%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -2,23 +2,27 @@ import java.util.Arrays;
 import java.util.HashMap;
 class Solution {
     public boolean isAnagram(String s, String t) {
-         char[] S = s.toCharArray();
-        char[] T = t.toCharArray();
+        char[] s1 = s.toCharArray();
+        char[] t1 = t.toCharArray();
 
-          if (S.length != T.length) return false;
+        if(s1.length != t1.length){
+            return false;
+        }
 
-        int[] freq = new int[26]; // For lowercase English letters
+        int[] freq = new int[26];
 
-        // Count characters from s and subtract for t
-        for (int i = 0; i < s.length(); i++) {
+        for(int i =0;i<s1.length;i++){
             freq[s.charAt(i) - 'a']++;
             freq[t.charAt(i) - 'a']--;
+
         }
 
-        // If all counts are zero, it's an anagram
-        for (int count : freq) {
-            if (count != 0) return false;
+        for(int count : freq){
+            if(count != 0){
+                return false;
+            }
         }
+
         return true;
 
     }
